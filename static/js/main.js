9144e4766b8e99e9a724dffc41341309b7a29a33
Mosesvalei <moses.mwanzia@students.moringaschool.com>
2021-06-13 18:54:49 +0300

    created js functions

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,11 @@
-let button = document.getElementById('btn');
-let birthYear = document.getElementById("year").value;
-let birthMonth = document.getElementById("month").value;
-let birthDay = document.getElementById("day").value;
-let male = document.getElementById("male")
-let female = document.getElementById("female")
+let button = document.getElementById("btn");
+let birthYear = document.getElementById("year");
+let birthMonth = document.getElementById("month");
+let birthDay = document.getElementById("day");
+let male = document.getElementById("male");
+let female = document.getElementById("female");
+var akan = document.querySelector(".akan");
 let mm;
 let cc;
 let dd;
-let yy
-
-
-function calcDayOfBirth() {
-    let mm = birthMonth;
-    let dd = birthDay;
-    let yearArr = birthYear.toString().split("");
-    let cc = parseInt(yearArr.slice(0, 2).join(""));
-    let yy = parseInt(yearArr.slice(2).join(""));
-    if (year >= 2000) {
-      yy = 100 + parseInt(yearArr.slice(2).join(""));
-    }
-    let d = Math.floor(
-      (cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7
-    );
-    return d;
-  }
+let yy;
